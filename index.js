c6dfd99e5f375c679a7348da799a79a39e21ed1d
Philipp Alferov <philipp.alferov@gmail.com>
2015-10-13 15:51:10 -0400

    Add gh pages support

--- index.js
+++ index.js
@@ -8,7 +8,11 @@
  * @return
  */
 
-module.exports = function isGithubUrl(url) {
-  var pattern = /(?:git|ssh|https?|git@[\w\.]+)?(?:\:\/\/)?github.com[\w\.@:\/~_-]+(?:\.git)?$/;
-  return pattern.test(url);
+module.exports = function isGithubUrl(url, options) {
+  options = options || {};
+  var domain = options.includeGhPages ? '(com|io)' : 'com';
+  var pattern = '(?:git|ssh|https?|git@[\\w\\.]+)?(?:\\:\\/\\/)?github.' + domain + '[\\w\\.@:\\/~_-]+(?:\\.git)?$';
+  var re = new RegExp(pattern);
+
+  return re.test(url);
 };
--- test/test.js
+++ test/test.js
@@ -13,24 +13,39 @@ var validUrls = [
   'www.github.com/facebook/react'
 ];
 
+var validGhPages = [
+  'http://alferov.github.io/',
+  'http://alferov.github.io/angular-file-saver/'
+];
+
 var invalidUrls = [
   'google.com',
   'https://google.com',
   'https://hello.github.com',
-  'unknown://github.com'
+  'unknown://github.com',
+  'http://alferov.github.io/'
 ];
 
 describe('is-github-url', function() {
+  describe('valid urls', function() {
+    validUrls.forEach(function(url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGithubUrl(url)).to.be.true;
+      });
+    });
 
-  validUrls.forEach(function(url) {
-    it('URL' + ' - ' + url + ' should be valid', function () {
-      expect(isGithubUrl(url)).to.be.true;
+    validGhPages.forEach(function(url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGithubUrl(url, { includeGhPages: true })).to.be.true;
+      });
     });
   });
 
-  invalidUrls.forEach(function(url) {
-    it('URL' + ' - ' + url + ' should be invalid', function () {
-      expect(isGithubUrl(url)).to.be.false;
+  describe('invalid urls', function() {
+    invalidUrls.forEach(function(url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGithubUrl(url)).to.be.false;
+      });
     });
   });
 
